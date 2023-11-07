1b31974c55a2127896bb4044585c0b6eb88079a0
Kimicu <kitgunplay@gmail.com>
2023-11-07 15:53:04 +0300

    feat: update for yandex games

---
+++ Assets/_Project/Scripts/ReadyApiYandex.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace FireAndWater
+{
+    public class ReadyApiYandex : MonoBehaviour
+    {
+        private static bool _isInvoke = false;
+
+        private void Start()
+        {
+            if (!_isInvoke)
+            {
+                #if UNITY_WEBGL
+                Agava.YandexGames.YandexGamesSdk.GameReady();
+                #endif
+                _isInvoke = true;
+            }
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/SceneLoader.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace FireAndWater
+{
+    public class SceneLoader : MonoBehaviour
+    {
+        public void LoadScene(int id)
+        {
+            SceneManager.LoadScene(id);
+        }
+    }
+}
